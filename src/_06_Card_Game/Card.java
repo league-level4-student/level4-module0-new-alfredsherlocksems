6e48c4d33722281af2ff22232906f7047b95aea2
student <student@ip-172-31-60-87.us-west-2.compute.internal>
2022-08-13 11:27:14 -0700

    nearly done card game

--- src/_06_Card_Game/Card.java
+++ src/_06_Card_Game/Card.java
@@ -9,6 +9,24 @@ public class Card {
         this.rank = rank;
         this.suit = suit;
     }
+    
+    enum Suit {
+    	HEARTS, CLUBS, SPADES, DIAMONDS
+    }
+    
+    enum Rank {
+    	TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
+    	public int value;
+    	
+    	private Rank (int value) {
+    		this.value = value;
+    	}
+    	
+    	public int getValue() {
+    		return value;
+    	}
+
+    }
 
     public Rank getRank() {
         return rank;
@@ -17,7 +35,8 @@ public class Card {
     public Suit getSuit() {
         return suit;
     }
-
+    
+  
     public String toString() {
         return rank + " of " + suit;
     }
--- src/_06_Card_Game/CardDealer.java
+++ src/_06_Card_Game/CardDealer.java
@@ -1,10 +1,124 @@
 package _06_Card_Game;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class CardDealer {
-    
-    ArrayList<Card> deck;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import _06_Card_Game.Card.Rank;
+import _06_Card_Game.Card.Suit;
+
+public class CardDealer implements ActionListener {
+    
+    ArrayList<Card> deck = new ArrayList<Card>();
+    Rank[] ranks = Rank.values();
+    Suit[] suit = Suit.values();
+    Card card;
+    Random ran = new Random();
+    ArrayList<Card> player  = new ArrayList<Card>();
+    ArrayList<Card> dealer = new ArrayList<Card>();
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JButton hit = new JButton();
+    JButton stand = new JButton();
+    JLabel card1 = new JLabel();
+    JLabel card2 = new JLabel();
+    
+    public void setup() {
+    	frame.setVisible(true);
+    	frame.setSize(400, 400);
+    	frame.add(panel);
+    	panel.add(hit);
+    	panel.add(stand);
+    	hit.setText("hit");
+    	stand.setText("stand");
+    	hit.addActionListener(this);
+    	stand.addActionListener(this);
+    }
+    
+    public void makeDeck() {
+    	for (int i = 0; i < 4; i++) {
+        	for (int j = 1; j <= ranks.length; j++) {
+        		card = new Card(ranks[j], suit[i]);
+        		deck.add(card);
+        	}
+        }
+    }
+    
+    public void shuffle() {
+    	Card value;
+    	ArrayList<Card> newDeck = new ArrayList<Card>();
+    	int random = 0;
+    	for(int i = 52; i > 0; i--) {
+    		random = ran.nextInt(i );
+    		newDeck.add(deck.get(random));
+    		deck.remove(random);
+    	}
+    	deck = newDeck;
+    }
+    
+    public void play() {
+    	player.add(deck.get(0));
+    	dealer.add(deck.get(1));
+    	player.add(deck.get(2));
+    	dealer.add(deck.get(3));
+    	
+    	//card1.setText(player.get(0).getSuit() + " " + player.get(0).getRank());
+    	cardDisplay(player.get(0), card1);
+    	cardDisplay(player.get(1), card2);
+    	
+    	frame.pack();
+    }
+    
+    public void winChecker() {
+    	int dealerTotal = 0;
+    	for (int i = 0; i < dealer.size(); i++) {
+    		dealerTotal+=dealer.get(i).getRank().getValue();
+    	}
+    	if (getPlayerTotal() > dealerTotal) {
+    		
+    	}
+    	else {
+    		
+    	}
+    }
+    
+    public void cardDisplay(Card card, JLabel label) {
+    	label.setText(card.getSuit() + " " + card.getRank());
+    }
+    
+    public int getPlayerTotal()
+    {
+    	int playerTotal = 0;
+    	for (int i = 0; i < player.size(); i++) {
+    		playerTotal+=player.get(i).getRank().getValue();
+    	}
+    	return playerTotal;
+    }
+    public static void main (String [] args) {
+    	CardDealer play = new CardDealer();
+    	play.setup();
+    	play.makeDeck();
+    	play.shuffle();
+    	play.play();
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getSource() == hit) {
+			shuffle();
+			player.add(deck.get(0));
+			
+		}
+		else if (arg0.getSource() == stand) {
+			winChecker();
+		}
+		frame.pack();
+	}
 }
